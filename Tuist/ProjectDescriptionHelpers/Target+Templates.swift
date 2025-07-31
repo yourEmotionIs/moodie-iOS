86e36c2e1dd67c2fa0e7029786a1b4cce63a9bd6
leesoongin <tnddls2ek@naver.com>
2025-08-01 01:08:39 +0900

    chore:  kakao, apple login Schemes, urlTypes, code sign 설정
    - main 앱 타겟에 kakao, apple login Schemes, urlTypes, code sign 설정
    - feature, testing, example module에 code sign 설정 추가

--- Tuist/ProjectDescriptionHelpers/Target+Templates.swift
+++ Tuist/ProjectDescriptionHelpers/Target+Templates.swift
@@ -16,7 +16,7 @@ public extension Target {
             name: name,
             destinations: .iOS,
             product: .app,
-            bundleId: "com.\(name).app",
+            bundleId: "com.mumu.\(name).app",
             deploymentTargets: .iOS(Project.depolymentTarget),
             infoPlist: .extendingDefault(with: [
                 "UILaunchStoryboardName": "LaunchScreen.storyboard",
@@ -27,10 +27,37 @@ public extension Target {
                     "UISceneConfigurations": [
                         "UIWindowSceneSessionRoleApplication": []
                     ]
+                ],
+                "LSApplicationQueriesSchemes": [
+                    "kakaokompassauth" // 카카오톡으로 로그인
+                ],
+                "CFBundleURLTypes": [
+                    // Apple 로그인
+                    [
+                        "CFBundleURLName": "AppleSignIn",
+                        "CFBundleURLSchemes": ["com.\(name).app"],
+                        "CFBundleURLRole": "Editor"
+                    ],
+                    // 카카오 로그인
+                    [
+                        "CFBundleURLName": "KakaoLogin",
+                        "CFBundleURLSchemes": ["kakaoc6e355b541377b0886d546a44fc76f50"], // 실제 카카오 앱 키
+                        "CFBundleURLRole": "Editor"
+                    ]
                 ]
             ]),
             sources: ["Sources/**"],
-            dependencies: dependencies
+            entitlements: .dictionary([
+                "com.apple.developer.applesignin": .array([.string("Default")])
+            ]),
+            dependencies: dependencies,
+            settings: .settings(
+                base: [
+                    "CODE_SIGN_IDENTITY": "Apple Development",
+                    "DEVELOPMENT_TEAM": "7W64WHVKVN",
+                    "CODE_SIGN_STYLE": "Automatic"
+                ]
+            )
         )
     }
     
@@ -49,8 +76,10 @@ public extension Target {
             infoPlist: infoPlist,
             sources: ["Sources/**"],
             resources: hasResource ? ["Resources/**"] : nil,
+            entitlements: .dictionary([
+                "com.apple.developer.applesignin": .array([.string("Default")])
+            ]),
             dependencies: dependencies
-            
         )
     }
     
@@ -66,7 +95,14 @@ public extension Target {
             deploymentTargets: .iOS(Project.depolymentTarget),
             infoPlist: .default,
             sources: ["Sources/**"],
-            dependencies: dependencies
+            dependencies: dependencies,
+            settings: .settings(
+                base: [
+                    "CODE_SIGN_IDENTITY": "Apple Development",
+                    "DEVELOPMENT_TEAM": "7W64WHVKVN",
+                    "CODE_SIGN_STYLE": "Automatic"
+                ]
+            )
         )
     }
     
@@ -98,7 +134,14 @@ public extension Target {
             deploymentTargets: .iOS(Project.depolymentTarget),
             infoPlist: .default,
             sources: ["Sources/**"],
-            dependencies: dependencies
+            dependencies: dependencies,
+            settings: .settings(
+                base: [
+                    "CODE_SIGN_IDENTITY": "Apple Development",
+                    "DEVELOPMENT_TEAM": "7W64WHVKVN",
+                    "CODE_SIGN_STYLE": "Automatic"
+                ]
+            )
         )
     }
     
@@ -130,7 +173,14 @@ public extension Target {
             deploymentTargets: .iOS(Project.depolymentTarget),
             infoPlist: .default,
             sources: ["Sources/**"],
-            dependencies: dependencies
+            dependencies: dependencies,
+            settings: .settings(
+                base: [
+                    "CODE_SIGN_IDENTITY": "Apple Development",
+                    "DEVELOPMENT_TEAM": "7W64WHVKVN",
+                    "CODE_SIGN_STYLE": "Automatic"
+                ]
+            )
         )
     }
 
